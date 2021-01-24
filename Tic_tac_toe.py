eff32470f461c6e31b32a08af23be058c7b9820e
KiR <borapechat@gmail.com>
2021-01-24 22:21:35 +0300

    Skillfactory: delete files add python

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -4,4 +4,20 @@ def startgreet():
     print('-----------------')
 
 field = [[' ']*3 for i in range(3)]
-print  (field)
+
+
+def show_field():
+    print('    1   2   3')
+    print('  -------------')
+    for i in range(3):
+        row = ' | '.join(field[i])
+        print(i+1, '|', row, '|',)
+        print('  -------------')
+
+def move():
+    x, y = map(int, input('Your move, \nplease enter the x and y coordinates separated by space:\n').split())
+    return x, y
+
+x, y = move()
+show_field()
+print(field[x][y])
