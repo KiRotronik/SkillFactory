ea828e6eb0bd861958a4a8f878935ae793034cf8
KiR <borapechat@gmail.com>
2021-01-26 18:30:38 +0300

    SkillFactrory: tic tak toe in progress

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -3,9 +3,6 @@ def startgreet():
     print('-- Tik Tac Toe --')
     print('-----------------')
 
-field = [[' ']*3 for i in range(3)]
-
-
 def show_field():
     print('    1   2   3')
     print('  -------------')
@@ -14,10 +11,22 @@ def show_field():
         print(i+1, '|', row, '|',)
         print('  -------------')
 
-def move():
-    x, y = map(int, input('Your move, \nplease enter the x and y coordinates separated by space:\n').split())
-    return x, y
+def enter_cords():
+    while True:
+        x, y = input('Enter the x and y coordinates separated by space:\n').split()
+        if x.isdigit() and y.isdigit():
+            if 0 < x < 4 and 0 < y < 4:
+                if field[x][y] == ' ':
+                    return x, y
+                else:
+                    print("Cell is occupied")
+            else:
+                print("Enter x and y greater than 0 and less than 4")
+        else:
+            print("Enter digits")
 
-x, y = move()
-show_field()
-print(field[x][y])
+field = [[' ']*3 for i in range(3)]
+field[1][2] = 'X'
+enter_cords()
+
+print(show_field())
