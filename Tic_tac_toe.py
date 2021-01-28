6ca8c454eea03b20864ad71958ae7f7f13931830
KiR <borapechat@gmail.com>
2021-01-28 16:14:35 +0300

    SkillFactory: update tic tak toe

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -2,6 +2,8 @@ def startgreet():
     print('-----------------')
     print('-- Tik Tac Toe --')
     print('-----------------')
+    print('Enter the x and y\n-- coordinates --\n--- separated ---\n--- by space! ---')
+    print('-----------------')
 
 def show_field():
     print('    1   2   3')
@@ -13,20 +15,48 @@ def show_field():
 
 def enter_cords():
     while True:
-        x, y = input('Enter the x and y coordinates separated by space:\n').split()
-        if x.isdigit() and y.isdigit():
-            if 0 < x < 4 and 0 < y < 4:
-                if field[x][y] == ' ':
-                    return x, y
-                else:
-                    print("Cell is occupied")
-            else:
-                print("Enter x and y greater than 0 and less than 4")
-        else:
+        cords = input().split()
+        if len(cords) != 2:
+            print("Enter two digits")
+            continue
+        x, y = cords
+        if not(x.isdigit()) or not(y.isdigit()):
             print("Enter digits")
+            continue
+        x, y = int(x), int(y)
+        if 1 > x or x > 3 or 1 > y or y > 3:
+            print("Enter x and y greater than 0 and less than 4")
+            continue
+        if field[x-1][y-1] != ' ':
+            print("Cell is occupied")
+            continue
+        else:
+            return x, y
+
+
+
+
+startgreet()
 
 field = [[' ']*3 for i in range(3)]
-field[1][2] = 'X'
-enter_cords()
 
-print(show_field())
+
+num_move = 0
+while True:
+    show_field()
+    num_move += 1
+    if num_move % 2 != 0:
+        print("X - move")
+    else:
+        print("O - move")
+    x, y = enter_cords()
+    if num_move % 2 != 0:
+        field[y-1][x-1] = 'X'
+    else:
+        field[y-1][x-1] = '0'
+    if num_move == 9:
+        print('Draw!!!')
+        break
+    
+
+
