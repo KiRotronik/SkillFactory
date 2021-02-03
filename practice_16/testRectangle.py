6bd30abe26a39bc55c2beb65e222739e298e8dcf
KiR <borapechat@gmail.com>
2021-02-03 16:45:50 +0300

    SkillFactory: add figures

---
+++ practice_16/testRectangle.py
@@ -0,0 +1,16 @@
+from rectangle import Rectangle
+
+r1 = Rectangle(10, 5)
+
+print(r1.width)
+print(r1.height)
+print(r1.get_width())
+print(r1.get_area())
+
+r2 = Rectangle(10, 5)
+
+print(r2.width)
+print(r2.height)
+print(r2.get_height())
+print(r2.get_width())
+print(r2.get_area())
