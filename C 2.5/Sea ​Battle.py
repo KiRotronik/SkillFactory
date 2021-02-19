9d8f8ba4bd534aab9f7ea1ee818b468b1fef62b3
KiR <borapechat@gmail.com>
2021-02-19 20:12:57 +0300

    SkillFctory: update Seabattle

--- C 2.5/Sea ​Battle.py
+++ C 2.5/Sea ​Battle.py
@@ -1,3 +1,9 @@
+import random
+
+
+class BoardOutException:
+    pass
+
 class Board:
     hid =
     def create_board(self,):
@@ -23,25 +29,64 @@ class Ship:
     def dots(self):
         return
 
-class Player:
+class User(Player):
     def ask(self):
-    def move(self):
+        while True:
+            cords = input("Enter x,y coordinates for shoot").split()
+            if len(cords) != 2:
+                print("Enter two digits from 1 to 6")
+                continue
+            x, y = cords
+            if not x.isdigit() and y.isdigit():
+                print("Enter two digits from 1 to 6")
+                continue
+            x, y = int(x), int(y)
+            if x < 1 or x > 6 and y < 1 or y > 6:
+                print("Enter two digits from 1 to 6")
+                continue
+            else:
+                return x, y
+    def move(self, x, y):
+        x, y = self.ask()
 
 class Ai(Player):
     def ask(self):
+        x, y = random.randint(0,6), random.randint(0,6)
+        return x, y
+
 class User(Player):
     def ask(self):
+        x, y = input("Enter x,y coordinates for shoot").split()
+        if not x.isdigit() and y.isdigit():
+            print("Enter two digit from 1 to 6")
+        if x < 1 or x > 6 and y < 1 or y > 6:
+            print("Enter two digit from 1 to 6")
+        else:
+            return x, y
+
+
 
 class Dot:
+    dots = [[O for j in range(6)]for i in range(6)]
     def coord(self, x, y):
-        self.x = x
-        self.y = y
+        self.cord = dots[y][x]
+
     def __eq__(self, other):
 
 class Game:
+    def welcome(self):
+        print("---------------")
+        print("--- Welcome ---")
+        print("----- to ------")
+        print("- SEA BATTLE --")
+        print("---- GAME! ----")
+        print("---------------")
+
     def random_board(self):
+
     def greet(self):
     def loop(self):
     def start(self):
 
-
+d = Dot()
+print(d)
--- practice/test_code.py
+++ practice/test_code.py
@@ -1,18 +1,31 @@
-class Moon:
-
-    def __eq__(self, other):
-        return self.value == other
+class Player:
 
 
-class Earth:
+    def move(self):
+        a, b = User.ask()
+        return a, b
 
-    def __eq__(self, other):
-        return self.value == other
 
 
-a = Earth()
-b = Moon()
+class User(Player):
+    @staticmethod
+    def ask():
+        while True:
+            cords = input("Enter x,y coordinates for shoot").split()
+            if len(cords) != 2:
+                print("Enter two digits from 1 to 6")
+                continue
+            x, y = cords
+            if not x.isdigit() and y.isdigit():
+                print("Enter two digits from 1 to 6")
+                continue
+            x, y = int(x), int(y)
+            if x < 1 or x > 6 and y < 1 or y > 6:
+                print("Enter two digits from 1 to 6")
+                continue
+            else:
+                return x, y
 
-a.value = 3
-b.value = 3
-print(a == b)
+print(Player.move())
+
+
