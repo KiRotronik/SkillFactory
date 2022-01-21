9797b3e1ff6b41a0f09dbb61f2e726bba6652ab6
Сергей Рычгорский <r.sergey@local.lan>
2022-01-21 19:46:25 +0300

    Try update repository

--- practice/test_code.py
+++ practice/test_code.py
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
 import requests
-import json
-
-data = {'key': 'value'}
-
-r = requests.post('https://httpbin.org/post', json=json.dumps(
-    data))  # отправляем пост-запрос, но только в этот раз тип передаваемых данных будет JSON
-print(r.content)
-=======
->>>>>>> f3b19fdf2b6459d6ea6e4d489f5e22e34c984bcb
+import bs4
--- practice/test_code2.py
+++ practice/test_code2.py
@@ -1,2 +1,12 @@
-array =  [i for i in range(1,100)]
-print(array)
+import requests
+
+r = requests.get(
+    'https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=html')  # делаем запрос на сервер по переданному адресу
+
+print(type(r.content))
+print(r.status_code)
+
+
+
+
+
